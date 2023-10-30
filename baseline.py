fc5f215c0bc19afcb30b36f4f184ea19b8cf236a
oledenhertog <ole.denhertog@student.hu.nl>
2023-10-30 11:59:34 +0100

    rmse ipv mse

--- baseline.py
+++ baseline.py
@@ -1,5 +1,6 @@
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import r2_score
+from math import sqrt
 
 def calculate_baseline(df):
     baseline = df['anm_tot_fh'].mean()
@@ -7,7 +8,7 @@ def calculate_baseline(df):
     y_pred = [baseline] * len(df)
     y_true = df['anm_tot_fh']
 
-    baseline_mse = mean_squared_error(y_true, y_pred)
+    baseline_rmse = sqrt(mean_squared_error(y_true, y_pred))
     baseline_r2 = r2_score(y_true, y_pred)
 
-    return baseline_mse, baseline_r2
+    return baseline_rmse, baseline_r2
