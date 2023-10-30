02167125a24d3797f0f0283ac0e6c9a15c92ba33
GuuscoNL <99217547+GuuscoNL@users.noreply.github.com>
2023-10-30 15:30:45 +0100

    GUI - comments

--- app.py
+++ app.py
@@ -9,28 +9,26 @@ class App(ctk.CTk):
     def __init__(self):
         super().__init__()
         
+        # Zet de cursor op watch (laad cursor)
         self.config(cursor="watch")
         self.update()
 
+        # Maak het window
         self.title("ProRail dashboard")
         self.geometry(f"{WINDOW_WIDTH}x{WINDOW_HEIGHT}")
         self.resizable(True, True)
         self.minsize(300, 400)
 
-
-        self.features_input_fields = {}
-    
+        # Maak de grid
         self.grid_columnconfigure(0, weight = 1)
         self.grid_rowconfigure(0, weight = 10)
         self.grid_rowconfigure(1, weight = 1)
         
-        # laad het model
-        with open("models/DecisionTreeRegressor.pkl", "rb") as file:
-            self.model = pickle.load(file)
+        self.features_input_fields = {}
+
+        self.load_data()
         
-         # Laad het model dat is gebruikt tijdens het fitten van het model
-        self.model_df_raw = pd.read_csv("data/model_df.csv", engine="pyarrow", index_col=0)
-        
+        # Main frames
         self.top_frame = ctk.CTkFrame(self)
         self.top_frame.grid(row = 0, column = 0, sticky = "nesw")
         self.top_frame.propagate(False)
@@ -59,28 +57,32 @@ class App(ctk.CTk):
         self.predict_button = ctk.CTkButton(self.result_frame, text="Voorspel", command=self.predict, font=("Arial", 18))
         self.predict_button.pack(side="bottom", pady=(0, 20))
         
+        # Zet de cursor op normaal
         self.config(cursor="")
         self.update()
-    
+
+
     def get_features(self):
         
-        # remove the target column and empty columns
-        model_df_copy = self.model_df_raw.copy()
-        model_df_copy = model_df_copy.drop(["anm_tot_fh"], axis=1)
+        # remove the target column
+        model_df_copy = self.model_df_raw.copy().drop(["anm_tot_fh"], axis=1)
         
         features = []
         
-        while len(model_df_copy.columns) > 1:
+        while len(model_df_copy.columns) > 0:
 
             first_colm = model_df_copy.columns[0]
 
+            # Krijg het begin van de naam van de kolom
             column_start = "_".join(first_colm.split("_")[:-1])
-            # get all columns that start with column var
+
+            # Krijg alle kolommen die beginnen met de naam van dat kolom
             columns = [x for x in model_df_copy.columns if x.startswith(column_start)]
             features.append({"name": column_start, 
                              "type": "option", 
                              "options": [x.split("_")[-1] for x in columns]})
             
+            # Verwijder de kolommen die al zijn toegevoegd en ga door naar de volgende kolom
             model_df_copy = model_df_copy.drop(columns, axis=1)
             
         return features
@@ -94,6 +96,7 @@ class App(ctk.CTk):
         label = ctk.CTkLabel(frame, text=f"{feature_name}:", font=("Arial", 18))
         label.pack(side="left", fill="x", padx=(WINDOW_WIDTH / 13, 0))
         
+        # Maak een input veld voor de feature gebaseerd op het type
         if feature_type == "str" or feature_type == "int":
             input_field = ctk.CTkEntry(frame, width=200)
 
@@ -112,9 +115,11 @@ class App(ctk.CTk):
         
         for feature in self.features:
             if feature["type"] == "option":
+                # Zet alle opties op False
                 for x in feature["options"]:
                     X[f"{feature['name']}_{x}"] = False
                 
+                # Zet de optie die is gekozen op True
                 value = self.features_input_fields[feature["name"]].get()
                 X[f"{feature['name']}_{value}"] = True
             else:
@@ -124,6 +129,14 @@ class App(ctk.CTk):
 
         predicted = self.model.predict(X)[0]
         self.result_duration_label.configure(text=f"Duur van storing: {round(predicted)} minuten")
+    
+    def load_data(self):
+        # laad het model
+        with open("models/DecisionTreeRegressor.pkl", "rb") as file:
+            self.model = pickle.load(file)
+        
+        # Laad het model dat is gebruikt tijdens het fitten van het model
+        self.model_df_raw = pd.read_csv("data/model_df.csv", engine="pyarrow", index_col=0)
 
 app = App()
 app.mainloop()
