9a97e435496b1a521826925e34e3a216c2e1fb8a
GuuscoNL <99217547+GuuscoNL@users.noreply.github.com>
2023-11-09 15:31:00 +0100

    GUI - fix bereken knop geo_code

--- app/visualization.py
+++ app/visualization.py
@@ -124,7 +124,7 @@ class VisualizationFrame(ctk.CTkFrame):
         geo_code = self.geo_code_entry.get()
         
         # Check of de geocode valid is
-        if  geo_code not in self.all_geo_codes:
+        if  geo_code not in self.all_geo_codes or geo_code == "":
             # zorg dat de geocode entry rood wordt
             if geo_code == "":
                 self.geo_code_entry.configure(text_color="white")
